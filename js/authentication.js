7f49996860a56d966eb90aa945baaac880f50b62
irvo11 <irin.vokopola@cit.edu.al>
2024-05-20 23:55:03 +0200

    third commit

--- js/authentication.js
+++ js/authentication.js
@@ -20,31 +20,67 @@ document.getElementById('login-link').addEventListener('click', function() {
     }, 2000);
 });
 
-$(document).ready(function() {
-    $('#loginBtn').click(function() {
-        var email = $('#Lemail').val();
-        var password = $('#Lpassword').val();
 
-        // Hash the password using SHA-256
-        var hashedPassword = CryptoJS.AES.encrypt(password, 'diploma').toString();
+document.getElementById('signupBtn').addEventListener('click', async function(event){
+    event.preventDefault(); // prevent form from submitting and refreshing the page
+    let name, lname, age, number, email, pass, gender;
 
-        console.log('Email:', email);
-        console.log('Password:', password);
-        console.log('Hashed Password:', hashedPassword);
+    name = document.getElementById("first-name").value;
+    lname = document.getElementById("last-name").value;
+    age = document.getElementById("age").value;
+    gender = document.getElementById("gender").value;
+    number = document.getElementById("phoneNumber").value;
+    email = document.getElementById("signup-email").value;
+    pass = document.getElementById("signup-password").value;
 
-        $.ajax({
-            type: 'POST',
-            url: '../php/login.php',
-            data: {
-                email: email, 
-                hashedPassword: hashedPassword
-            },
-            success: function(response){
-                console.log(response);
-            },
-            error: function(){
-                console.log('Error occurred during AJAX request');
-            }
-        });
-    });
+    var hashedPassword = CryptoJS.AES.encrypt(pass, 'diploma').toString();
+
+    const data = {name, lname, age, gender, number, email, pass, hashedPassword};
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+    const response = await fetch("/api", options);
+    const json = await response.json();
+    console.log(json);
 });
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//     $('#loginBtn').click(function() {
+//         var email = $('#Lemail').val();
+//         var password = $('#Lpassword').val();
+
+//         // Hash the password using SHA-256
+//         var hashedPassword = CryptoJS.AES.encrypt(password, 'diploma').toString();
+
+//         console.log('Email:', email);
+//         console.log('Password:', password);
+//         console.log('Hashed Password:', hashedPassword);
+
+//         $.ajax({
+//             type: 'POST',
+//             url: '../php/login.php',
+//             data: {
+//                 email: email, 
+//                 hashedPassword: hashedPassword
+//             },
+//             success: function(response){
+//                 console.log(response);
+//             },
+//             error: function(){
+//                 console.log('Error occurred during AJAX request');
+//             }
+//         });
+//     });
+// });
+
---
+++ js/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+app.listen(4000, () => console.log('listening at 4000'));
+app.use(express.static('..html'));
+app.use(express.json({ limit: '1mb' }));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+  });
